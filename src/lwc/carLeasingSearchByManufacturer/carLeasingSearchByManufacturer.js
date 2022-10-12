1701ab4883871f0f4d44d6549392c1a93ad83069
Przemysław Bąbel <86799399+BabelWebDeveloper@users.noreply.github.com>
2022-10-12 08:26:45 +0200

    Added search engine that search by manufacturer

---
+++ src/lwc/carLeasingSearchByManufacturer/carLeasingSearchByManufacturer.js
@@ -0,0 +1,34 @@
+import {LightningElement, wire, track} from 'lwc';
+import {publish, MessageContext} from 'lightning/messageService';
+import carLeasingSearchChannel from '@salesforce/messageChannel/carLeasingSearchChannel__c';
+
+export default class CarLeasingSearchBYManufacturer extends LightningElement {
+    searchKey;
+
+    @track
+    product2s;
+
+    @wire(MessageContext)
+    messageContext;
+
+    isLoading = false;
+
+    handleLoading() {
+        this.isLoading = true;
+    }
+
+    handleDoneLoading() {
+        this.isLoading = false;
+    }
+
+    handleSearchKey(event) {
+        this.searchKey = event.target.value;
+    }
+
+    setSearchKey() {
+        const payload = {
+            searchKeyManufacturer: this.searchKey
+        };
+        publish(this.messageContext, carLeasingSearchChannel, payload);
+    }
+}
---
+++ src/lwc/carLeasingSearchResults/carLeasingSearchResults.js
@@ -0,0 +1,76 @@
+import {LightningElement, track, wire, api} from 'lwc';
+import {subscribe, publish, MessageContext} from 'lightning/messageService';
+import carLeasingSearchChannel from '@salesforce/messageChannel/carLeasingSearchChannel__c';
+import findpricebookEntriesByName from '@salesforce/apex/CarLeasingExperienceCloudController.searchCarsByName';
+
+export default class CarLeasingSearchResults extends LightningElement {
+    subscription = null;
+
+    @wire(MessageContext)
+    messageContext;
+
+    searchKey;
+
+    @track
+    pricebookEntries;
+    isLoading = false;
+
+    @api
+    selectedCarId;
+
+    @track
+    emptyResults = false;
+
+    connectedCallback() {
+        this.subscribeFromMessageChannel();
+    }
+
+    subscribeFromMessageChannel() {
+        this.subscription = subscribe(
+            this.messageContext,
+            carLeasingSearchChannel,
+            (message) => this.handleMessage(message)
+        );
+    }
+
+    handleMessage(message) {
+        this.isLoading = true;
+        this.notifyLoading(this.isLoading);
+        this.searchKey = message.searchKeyManufacturer;
+    }
+
+    @wire(findpricebookEntriesByName, {carNameSearchKey: '$searchKey'})
+    wiredCars(result) {
+        this.pricebookEntries = result;
+        this.notifyLoading(false);
+        this.checkSizeOfResults(result);
+    }
+
+    notifyLoading(isLoading) {
+        if (isLoading) {
+            this.dispatchEvent(new CustomEvent('loading'));
+        } else {
+            this.dispatchEvent(new CustomEvent('doneloading'));
+        }
+    }
+
+    checkSizeOfResults(result) {
+        if (result.data !== undefined) {
+            if (result.data.length < 1) {
+                this.emptyResults = true;
+            }
+            else {
+                this.emptyResults = false;
+            }
+        }
+    }
+
+    updateSelectedTile(event) {
+        this.selectedCarId = event.detail.carId;
+        this.sendMessageService(this.selectedCarId)
+    }
+
+    sendMessageService(carId) {
+        publish(this.messageContext, carLeasingSearchChannel, {recordId: carId});
+    }
+}
